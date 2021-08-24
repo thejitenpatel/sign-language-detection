e86bf0a4657d796ada63e4e9bf195a630f951e43
Elliot <elliot@Elliots-MacBook-Pro.local>
2021-08-24 22:34:56 +0530

    drawing utils

--- model.py
+++ model.py
@@ -11,14 +11,21 @@ mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
 def mediapipe_detection(image, model):
-    print("[INFO] Mediapipe detection")
+    # print("[INFO] Mediapipe detection")
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image.flags.writeble = False
+    image.flags.writeable = False
     results = model.process(image)
-    print("[INFO] model Processed: {}".format(results))
+    # print("[INFO] model Processed: {}".format(results))
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
+
+def draw_landmarks(frame, results):
+    mp_drawing.draw_landmarks(frame, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
+    mp_drawing.draw_landmarks(frame, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+    mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
 
 def detection():
@@ -31,7 +38,8 @@ def detection():
                 print("[INFO] Ignoring empty camera frame.")
                 continue
             image, results = mediapipe_detection(frame, holistic)
-            print(results)
+            draw_landmarks(image, results)
+            # print(results)
             cv2.imshow("frame", frame)
             if cv2.waitKey(10) & 0xFF == ord("q"):
                 break
